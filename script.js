7fadde8a64b61d7eef99e6392d31bd3857971c2c
linyuxuanlin <824676271@qq.com>
2023-07-31 20:20:22 +0800

    test

--- script.js
+++ script.js
@@ -20,3 +20,29 @@ fetch('info.json')
             });
     })
     .catch(error => console.error('Error fetching personal info:', error));
+
+
+const printButton = document.getElementById('printButton');
+
+printButton.addEventListener('click', () => {
+    window.print(); // 调用浏览器的打印功能
+});
+
+
+const generatePdfButton = document.getElementById('generatePdfButton');
+
+generatePdfButton.addEventListener('click', () => {
+  const doc = new jsPDF();
+  const pdfFileName = 'my_resume.pdf'; // 设置PDF文件名
+
+  // 获取页面内容
+  const pageContent = document.documentElement;
+
+  // 将页面内容添加到PDF
+  doc.html(pageContent, {
+    callback: function () {
+      // 保存PDF为文件
+      doc.save(pdfFileName);
+    }
+  });
+});
